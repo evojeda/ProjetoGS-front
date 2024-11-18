389bf3d1860966641436937210e022a828b5b3c1
Milena <milena.cod@gmail.com>
2024-11-18 19:35:27 -0300

    criando e estilizando a pag Sobre

---
+++ src/css/SobreStyle.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const SobreStyle=styled.h1`
+.container {
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  width: 80%;
+  margin: auto;
+}
+
+.header h2 {
+  color: #00ff00; /* Verde */
+  font-size: 24px;
+}
+
+.header h3 {
+  color: #00ff00;
+  font-size: 20px;
+}
+
+.content p {
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+.quiz {
+  margin-top: 20px;
+  padding: 15px;
+  border: 2px solid #00ff00;
+  border-radius: 8px;
+  text-align: center;
+}
+
+.quiz h2 {
+  color: #00ff00;
+}
+
+.quiz p {
+  font-size: 18px;
+}
+
+`
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {SobreStyle} from "../css/SobreStyle"
 
 const Sobre = () => {
   return (
-    <div>Sobre</div>
+    <SobreStyle>
+    <div className="container">
+      <div className="header">
+        <h2>Sobre Energia Renovável:</h2>
+        <h3>Oportunidades para um Futuro Sustentável</h3>
+      </div>
+      <div className="content">
+        <p>
+          Este tema foi proposto pela faculdade FIAP em parceria com Vanessa Grunwald.
+          A energia renovável segue tendências transformadoras, como a digitalização, que facilita o
+          gerenciamento remoto e a eficiência; a descentralização, que amplia o acesso a regiões isoladas;
+          a descarbonização, focada em reduzir as emissões de carbono; e a democratização, que permite
+          que cada vez mais pessoas gerem sua própria energia, criando “prosumidores” no sistema.
+        </p>
+        <p>
+          Essas tendências e desafios evidenciam o potencial da energia renovável como uma solução
+          sustentável e acessível para todos. Com o apoio contínuo da inovação, regulamentação e
+          infraestrutura, a transição energética promete transformar o setor e construir um futuro
+          verdadeiramente verde e responsável.
+        </p>
+      </div>
+      <div className="quiz">
+        <h2>Quiz Game</h2>
+        <p>Pergunta</p>
+        <p>Resposta</p>
+      </div>
+    </div>
+    </SobreStyle>
   )
 }
 
